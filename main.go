b60e97e43f054184b5eaf99fce504eec1729ab79
Dani <daniel.cr.ht@gmail.com>
2020-04-05 11:42:45 +0200

    Pass gofmt

--- main.go
+++ main.go
@@ -1,40 +1,40 @@
 package main
 
 import (
-  "github.com/gin-gonic/gin"
-  "log"
-  "net/http"
-  "os"
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+	"os"
 )
 
 func main() {
-  router := gin.Default()
+	router := gin.Default()
 
-  user := os.Getenv("USER")
-  password := os.Getenv("PASSWORD")
+	user := os.Getenv("USER")
+	password := os.Getenv("PASSWORD")
 
-  var authorized *gin.RouterGroup
-  if (user != "" && password != "") {
-    log.Printf("* PROTECTED BY USER AND PASSWORD *")
+	var authorized *gin.RouterGroup
+	if user != "" && password != "" {
+		log.Printf("* PROTECTED BY USER AND PASSWORD *")
 
-    authorized = router.Group("/", gin.BasicAuth(gin.Accounts{
-      user: password,
-    }))
-  } else {
-    log.Printf("* OPEN ACCESS *")
+		authorized = router.Group("/", gin.BasicAuth(gin.Accounts{
+			user: password,
+		}))
+	} else {
+		log.Printf("* OPEN ACCESS *")
 
-    authorized = router.Group("/")
-  }
+		authorized = router.Group("/")
+	}
 
-  // / open endpoint
-  router.GET("/", func(c *gin.Context) {
-    c.String(http.StatusOK, "你好\nHello\nHola")
-  })
+	// / open endpoint
+	router.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "你好\nHello\nHola")
+	})
 
-  // /pdf protected endpoint
-  authorized.GET("/pdf", func(c *gin.Context) {
-    c.JSON(http.StatusOK, gin.H{"authorized": "yes"})
-  })
+	// /pdf protected endpoint
+	authorized.GET("/pdf", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"authorized": "yes"})
+	})
 
-  router.Run()
+	router.Run()
 }
